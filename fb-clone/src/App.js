0d88632e57e80f6fd179074cbcea42c2e4286856
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-05-30 17:46:52 +0100

    logo exposure

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import React from "react";
+import Header from './Header';
 function App() {
   return (
     <div className="app">
       <h1>Let's build the facebook clone</h1>
-      <Header/>
+      
+      <Header />
     </div>
   );
 }
--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ".Header.css"
+import "./Header.css"
 function Header() {
   return (
     <div className='header'> 
       <div className='header__left'></div>
       <div className='header__middle'>
-      <img src="#"></img>
+      <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt=""></img>
       </div>
       <div className='header__right'></div>
     </div>
