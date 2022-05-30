7f8d46bbaf2ff8d6090c5a7820a585fdc3ef63c0
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-05-30 18:18:31 +0100

    fixed materialUI V4

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import "./Header.css"
-function Header() {
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+      function Header() {
   return (
     <div className='header'> 
       <div className='header__left'></div>
       <div className='header__middle'>
-      <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt=""></img>
+      <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt=""/>
+      <div className='header__input'>
+        
+        <SearchIcon />
+        <input type="text" />
+      </div>
       </div>
       <div className='header__right'></div>
     </div>
