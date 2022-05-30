f754cd99cfe0353587a5167c84193aa67b03fc7b
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-05-30 17:26:36 +0100

    exposing header

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="app">
       <h1>Let's build the facebook clone</h1>
+      <Header/>
     </div>
   );
 }
--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
-function  () {
+import ".Header.css"
+function Header() {
   return (
-    <div> </div>
+    <div className='header'> 
+      <div className='header__left'></div>
+      <div className='header__middle'>
+      <img src="#"></img>
+      </div>
+      <div className='header__right'></div>
+    </div>
   )
 }
 
-export default
+export default Header;
