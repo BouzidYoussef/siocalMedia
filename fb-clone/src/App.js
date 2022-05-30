b62e8d9aa3baeaf18ac16483541c119935e5d9b1
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-05-30 23:18:46 +0100

    ui exposure

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -4,8 +4,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Let's build the facebook clone</h1>
-      
       <Header />
     </div>
   );
--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -2,10 +2,19 @@ import React from 'react';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';      
-function Header() {
+import CallToActionIcon from '@material-ui/icons/CallToAction';    
+import SettingsInputSvideoIcon from '@material-ui/icons/SettingsInputSvideo';
+import FaceIcon from '@material-ui/icons/Face';
+
+    function Header() {
   return (
     <div className='header'> 
-      <div className='header__left'></div>
+      <div className='header__left'>
+        <div className='header__info'>
+          <FaceIcon />
+          <h4>Youssef Bouzid</h4>
+        </div>
+      </div>
       <div className='header__middle'>
       <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt=""/>
       <div className='header__input'>
@@ -16,7 +25,13 @@ function Header() {
       </div>
       <div className='header__right'>
         <div className='header__option'>
-          <HomeIcon fontSize="large" />
+          <HomeIcon fontSize="large" /> 
+        </div>
+        <div className='header__option'>
+          <CallToActionIcon />
+        </div>
+        <div className='header__option'>
+          <SettingsInputSvideoIcon />
         </div>
       </div>
     </div>
