4889d40e83d9f073a8187d32d83a6232c24ee3e0
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-06-10 21:37:34 +0100

    usable ui

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -7,7 +7,7 @@ function Feed() {
     <div className='feed'>
       <MessageSender />
       <Post 
-      profilePic="https://tn.linkedin.com/in/youssef-bouzid-338381222"
+      profilePic="https://scontent-pmo1-1.xx.fbcdn.net/v/t39.30808-6/278863611_5198555353516398_7926664748751654721_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LWsAn0kxlUcAX_c5E-X&_nc_ht=scontent-pmo1-1.xx&oh=00_AT8bK_JZzcmpfQTJDDwUEpob4pagAhaGV4T0S8zCyYdW0Q&oe=62A864CC"
       message="dar hedi"
       timestamp="this is a timestamp"
       username="Bouzid Youssef"
--- fb-clone/src/Sidebar.js
+++ fb-clone/src/Sidebar.js
@@ -6,11 +6,11 @@ import FiberNewIcon from '@material-ui/icons/FiberNew';
 import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 
-let pic = [];
+
 function Sidebar() {
   return (
     <div className='sidebar'> 
-    <SideBarRow  src="./fb-image" 
+    <SideBarRow  src="https://scontent-pmo1-1.xx.fbcdn.net/v/t39.30808-6/278863611_5198555353516398_7926664748751654721_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LWsAn0kxlUcAX_c5E-X&_nc_ht=scontent-pmo1-1.xx&oh=00_AT8bK_JZzcmpfQTJDDwUEpob4pagAhaGV4T0S8zCyYdW0Q&oe=62A864CC" 
  title="Bouzid Youssef" />
     <SideBarRow Icon={WhatshotIcon} title="Tendance" />
     <SideBarRow Icon={FiberNewIcon} title="dernières nouvelles"/>
